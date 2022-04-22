a6c77c9f79183ce71b40f2e75719002d646b5065
adrian <adrian.juszczak@umlaut.com>
2022-04-22 16:41:46 +0200

    redundant files removed

--- helper_scripts/add_ssh.sh
+++
@@ -1,9 +0,0 @@
-# ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key
-
-PASSPHASE_1 = ''
-PASSPHASE_2 = ''
-
-echo 'Generating public/private dsa key pair.'
-echo -ne '\n' | read -p 'Enter passphrase (empty for no passphrase):' 
-echo 'test'
-echo -ne '\n' | read -p 'Enter same passphrase again:'
--- helper_scripts/show_uid.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo 'This script shows uid'
-echo "Your UID is: ${UID}"
-
-
